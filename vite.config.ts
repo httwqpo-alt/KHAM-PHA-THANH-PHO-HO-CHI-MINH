3450c2a0432694a9700f5c214c76ef2c48f51fce
httwqpo-alt <htt.wqpo@gmail.com>
2026-09-12 08:43:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/KHAM-PHA-THANH-PHO-HO-CHI-MINH/',
+    base: '/KHAMPHATHANHPHOHOCHIMINH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
