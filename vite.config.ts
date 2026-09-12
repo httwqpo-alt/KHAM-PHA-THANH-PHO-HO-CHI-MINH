be180b6475b1e518f294e1db041bda5f868b3f74
httwqpo-alt <htt.wqpo@gmail.com>
2026-09-12 08:36:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/KHAM PHA THANH PHO HO CHI MINH/',
+    base: '/KHAM-PHA-THANH-PHO-HO-CHI-MINH/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
